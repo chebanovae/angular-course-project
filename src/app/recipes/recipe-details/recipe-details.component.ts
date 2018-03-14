f970d634e7802487ea881b239dd1246eca5cb632
Lisa <chebanovae@gmail.com>
2018-03-14 23:41:29 +0100

    Work in progress

--- src/app/recipes/recipe-details/recipe-details.component.ts
+++ src/app/recipes/recipe-details/recipe-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-details',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-details.component.css']
 })
 export class RecipeDetailsComponent implements OnInit {
+  currentRecipe: Recipe;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    console.log('RecipeDetailsComponent: recipe selected');
+    this.currentRecipe = recipe;
   }
 
 }
