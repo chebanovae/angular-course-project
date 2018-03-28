b7625e319c4fd2c2d73aa9c891af00de2c88c17f
Lisa <cheye03@ca.com>
2018-03-28 08:55:16 +0200

    Work in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { WelcomeRecipeComponent } from './recipes/welcome-recipe/welcome-recipe.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/recipe.service';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
